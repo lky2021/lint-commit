509485cd0cfad5f2989d383122e51e992ff23a6c
User likaiyuan <temp-email-for-phone-7dc74490-c3a7-4a87-907a-181507c93fdd@users.noreply.jihulab.com>
2026-01-29 22:10:50 +0800

    feat: 新增pinia

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCountStore } from '@/store/count.ts'
+
+const store = useCountStore()
+
+const handleClick = () => {
+  store.increment()
+}
+</script>
 
 <template>
-  <div class="text-[#ff0000]">123</div>
+  <button class="rounded bg-blue-500 px-4 py-2 text-white" @click="handleClick">
+    点击我{{ store.count }}
+  </button>
 </template>
 
 <style scoped></style>
