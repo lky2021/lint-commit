af631b412b4a31f36871adef27ad02e1c3d0065b
User likaiyuan <temp-email-for-phone-7dc74490-c3a7-4a87-907a-181507c93fdd@users.noreply.jihulab.com>
2026-01-31 17:09:27 +0800

    fix: 修复路径错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/lint-commit/',
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  }
 })
